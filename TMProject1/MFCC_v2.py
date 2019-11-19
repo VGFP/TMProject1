86d1d8b2606b9b782c8ba5a305c7f023f59463da
Filip Przychodni <pierwiastekz3@interia.pl>
2019-11-19 22:06:44 +0100

    Fixes

--- TMProject1/MFCC_v2.py
+++ TMProject1/MFCC_v2.py
@@ -3,9 +3,12 @@ import glob
 from scipy.io import wavfile
 import numpy as np
 import pickle
+import copy
+import os
+
 def mfcc_to_pickle():
     #reading wave files from path and adding to list
-    file_list = glob.glob("D:/Inne Projekty z Programowania/TMProject1/train/*.wav")
+    file_list = glob.glob("C:/Users/cjy09c/Desktop/Nowy_folder/TMProject1-master/train/*.wav")
     only_name = []
     for filename in file_list:
         temp = filename.split("\\")
@@ -14,7 +17,7 @@ def mfcc_to_pickle():
     #iterating through list
     mfcc_file_dict = {}
     for wave_name in only_name:
-        fs, data = wavfile.read("D:/Inne Projekty z Programowania/TMProject1/train/"+wave_name)
+        fs, data = wavfile.read("C:/Users/cjy09c/Desktop/Nowy_folder/TMProject1-master/train/"+wave_name)
 
         #creating MFCC matrix
         temp_name = wave_name.split('.')#cutting off extension
@@ -46,16 +49,18 @@ def load_mfcc_from_pickle():
 
 #mfcc_to_pickle()
 main_dict=load_mfcc_from_pickle()
-#print(np.shape(main_dict[1]))
+for k in range(10):
+    print(len(main_dict[k]))
 
 #keras model :)
 from keras.models import Sequential
 from keras.layers import Dense, Activation
 from keras.optimizers import Adam
+from keras.utils import to_categorical
 
 NB_OUTPUTS=10
 NB_EPOCH = 20
-BATCH_SIZE = 32
+BATCH_SIZE = 15
 OPTIMIZER = Adam() # optimizer
 #N_HIDDEN = 128
 
@@ -63,7 +68,7 @@ model=Sequential()
 #add input layer with 13 neuron an 13 inputs with randomised weights at start with range from -0.05 to 0,05
 model.add(Dense(13, input_shape=(13,), kernel_initializer='random_uniform'))
 model.add(Activation('relu'))
-model.add(Dense(NB_CLASSES))
+model.add(Dense(NB_OUTPUTS))
 model.add(Activation('softmax'))
 model.summary()
 #compiling model so it can be executed by Tensorflow backend
@@ -72,24 +77,54 @@ model.compile(loss='categorical_crossentropy', optimizer=OPTIMIZER, metrics=['ac
 
 def split_speakers(dictionary, number):
     test_dict={0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: []}
-    fit_dict={0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: []}
-
+    #fit_dict={0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: []}
+    fit_dict = copy.deepcopy(dictionary)
+    print("Fit size: "+str(len(fit_dict[0])))
     for x in range(10):
-        test_dict[x]=dictionary[x][4*number:4*number+4]
-        fit_dict[x]=dictionary[x]
-        del fit_dict[x][4*number:4*number+4]
+        test_dict[x]=fit_dict[x][4*number:4*number+4]
+        k=4*number
 
+        del fit_dict[x][k:k+4]
+
+       #print("fit dict size: " + str(len(fit_dict[x])))
+        # for z in range(4):
+        #     print("k+z:"+str(k+z))
+        #     print("fit dict size: "+str(len(fit_dict[x])))
+        #     del fit_dict[x][k+z]
+    print("Group Number: "+str(number))
+    print("fit dict len for 0: "+str(len(fit_dict[0])))
+    print("test dict len for 0: " + str(len(test_dict[0])))
     return fit_dict, test_dict
 
+#n1,n2=split_speakers(main_dict,2)
 
 for test_group in range(5):
     fit_d,test_d=split_speakers(main_dict,test_group)
     for fit_number in range(10):
-        model.fit(fit_d[fit_number],fit_number)
+        for speakers in range(18):
+            #print("Test group: "+str(test_group))
+            #print("Fit number: "+str(fit_number))
+            #print("Speaker number (should be 18): "+str(len(fit_d[fit_number])))
+            fitData=np.array(fit_d[fit_number][speakers])
+            m,n=np.shape(fitData)
+            #print("m: "+str(m))
+            y_train =np.zeros((m,10))
+            for k in range(m):
+                y_train[k,fit_number]=1
+            print(np.ndim(y_train))
+            #y_train = to_categorical(y_train)
+            model.fit(fitData,y_train)
     for fit_number in range(10):
-        score = model.evaluate(test_d[fit_number], fit_number, verbose=1)
-        print("\n")
-        print("Test group: "+str(test_group)+" number: "+str(fit_number) )
-        print("Test score:", score[0])
-        print('Test accuracy:', score[1])
-        print("\n")
+        for speakers in range(4):
+            testData=np.array(test_d[fit_number][speakers])
+            m,n=np.shape(testData)
+            y_test=np.zeros((m,10))
+            for k in range(m):
+                y_test[k,fit_number]=1
+            score = model.evaluate(testData, y_test, verbose=1)
+            print("\n")
+            print("Test group: "+str(test_group+1)+" number: "+str(fit_number)+" speaker: "+str(speakers))
+            print("Test score:", score[0])
+            print('Test accuracy:', score[1])
+            print("\n")
+    clear = lambda: os.system('cls')
